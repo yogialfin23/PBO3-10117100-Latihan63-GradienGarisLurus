9a9517b53c0b9d29120c1e8b3a90770c0e901429
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-10-29 23:04:47 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Yogie Alfin Salim
+ * 10117100
+ * IF-3
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Koordinat k1 = new Koordinat(2, 10, 5, 7);
+        Koordinat k2 = new Koordinat(5, 1, 3, 12);
+
+        System.out.println("Garis yang melalui titik (2,10) dan (5,7) "
+                + "memiliki gradien sebesar " + k1.hitungGradien());
+        System.out.println("Garis yang melalui titik (5,1) dan (3,12) "
+                + "memiliki gradien sebesar " + k2.hitungGradien());
+    }
+}
